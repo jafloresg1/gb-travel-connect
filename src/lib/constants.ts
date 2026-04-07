d2fba8b04a655aabb1312b186415c0fde693f450
jafloresg1 <jafloresg1@gmail.com>
2026-04-06 22:07:50 -0500

    Update contact constants for social media and address

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,19 +4,19 @@
 // ============================================================
 
 /** WhatsApp number in international format without spaces or symbols */
-export const WHATSAPP_NUMBER = "521234567890";
+export const WHATSAPP_NUMBER = "+52818-210-5791";
 
 /** Full URL to your Facebook page */
-export const FACEBOOK_URL = "https://www.facebook.com/gbtravel";
+export const FACEBOOK_URL = "https://www.facebook.com/gbtravel.mx";
 
 /** Contact email */
-export const EMAIL = "info@gbtravel.mx";
+export const EMAIL = "gbtravelmty@gmail.com";
 
 /** Phone number displayed on the site */
-export const PHONE = "+52 1 234 567 890";
+export const PHONE = "+528122826931";
 
 /** Site address (physical) */
-export const ADDRESS = "Ciudad de México, México";
+export const ADDRESS = "San Pedro Garza Garcia,Nuevo Leon, México";
 
 /** WhatsApp link helper */
 export const whatsappLink = (message = "Hola, me interesa cotizar un viaje con GB Travel") =>
